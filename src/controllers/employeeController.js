4ecc7060b6278d94c195c5356fa026ee101af305
alvinxrww <alvin.xavier@ui.ac.id>
2025-01-26 15:34:19 +0700

    feat: update employee

--- src/controllers/employeeController.js
+++ src/controllers/employeeController.js
@@ -32,3 +32,16 @@ exports.getEmployees = async (req, res) => {
         res.status(500).json({ error: err.message });
     }
 };
+
+exports.updateEmployee = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { name, position, department, joined_at, status } = req.body;
+        const employee = await User.findByPk(id);
+        if (!employee) return res.status(404).json({ error: 'Employee not found' });
+        await employee.update({ name, position, department, joined_at, status });
+        res.status(200).json({ message: 'Employee updated successfully', employee });
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+};
--- src/routes/employeeRoutes.js
+++ src/routes/employeeRoutes.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const { authenticate } = require('../middlewares/auth');
 const { authorizeAdmin } = require('../middlewares/authorize');
-const { getEmployees, addEmployee } = require("../controllers/employeeController");
+const { getEmployees, addEmployee, updateEmployee } = require("../controllers/employeeController");
 
 const router = express.Router();
 
-router.get("/employee", authenticate, authorizeAdmin, getEmployees);
 router.post("/employee", addEmployee);
+router.get("/employee", authenticate, authorizeAdmin, getEmployees);
+router.put("/employee/:id", authenticate, authorizeAdmin, updateEmployee);
 
 module.exports = router;
