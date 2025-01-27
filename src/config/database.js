9b378e437cdff6b6c234d2cf87ee6a5441f8fa00
alvinxrww <alvin.xavier@ui.ac.id>
2025-01-27 16:18:24 +0700

    fix: adjust db config to cloud sql

--- src/config/database.js
+++ src/config/database.js
@@ -2,25 +2,28 @@ const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
 // Employee database
-const sequelize = new Sequelize(
-    process.env.DB_NAME, 
-    process.env.DB_USER, 
-    process.env.DB_PASSWORD, 
-    {
-        host: process.env.DB_HOST,
-        dialect: 'mysql',
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
+  dialect: 'mysql',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false // Required for self-signed certs
     }
-);
+  },
+  logging: true // Set true for debugging
+});
 
-// Attendance database
-const sequelize1 = new Sequelize(
-    process.env.DB1_NAME, 
-    process.env.DB1_USER, 
-    process.env.DB1_PASSWORD, 
-    {
-      host: process.env.DB1_HOST,
-      dialect: 'mysql',
+const sequelize1 = new Sequelize(process.env.DB1_NAME, process.env.DB1_USER, process.env.DB1_PASSWORD, {
+  host: process.env.DB_HOST,
+  dialect: 'mysql',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
     }
-  );
+  },
+  logging: true
+});
 
 module.exports = { sequelize, sequelize1 };
