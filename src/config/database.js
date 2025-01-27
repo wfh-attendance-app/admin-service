b045141c3bcfcc43f2b3f063a00d3cb70af0241d
alvinxrww <alvin.xavier@ui.ac.id>
2025-01-27 16:22:56 +0700

    docs: db config naming in comment

--- src/config/database.js
+++ src/config/database.js
@@ -14,6 +14,7 @@ const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, proces
   logging: true // Set true for debugging
 });
 
+// Attendance database
 const sequelize1 = new Sequelize(process.env.DB1_NAME, process.env.DB1_USER, process.env.DB1_PASSWORD, {
   host: process.env.DB_HOST,
   dialect: 'mysql',
