a40b6dd1b3d0c14579140d237f4ff8429f4cb1b1
alvinxrww <alvin.xavier@ui.ac.id>
2025-04-13 15:11:28 +0700

    fix: local setups

--- src/config/database.js
+++ src/config/database.js
@@ -4,27 +4,13 @@ require('dotenv').config();
 // Employee database
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
-  dialect: 'mysql',
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false // Required for self-signed certs
-    }
-  },
-  logging: true // Set true for debugging
+  dialect: 'postgres'
 });
 
 // Attendance database
 const sequelize1 = new Sequelize(process.env.DB1_NAME, process.env.DB1_USER, process.env.DB1_PASSWORD, {
   host: process.env.DB_HOST,
-  dialect: 'mysql',
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false
-    }
-  },
-  logging: true
+  dialect: 'postgres'
 });
 
 module.exports = { sequelize, sequelize1 };
